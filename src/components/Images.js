3490707fdec9707318031f9d8f6aab4b6c819caf
Zach Heckert <zach@zachheckert.com>
2021-11-02 16:57:14 -0600

    Strip away all React and replace with barebones html and css. Working commit.

--- src/components/Images.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export const Images = (props) => {
-    return(
-        <div className="centerAltImage">
-            <img className="imageStyle" src={props.src} alt={props.alt}/>
-        </div>
-    )
-}
